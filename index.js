c4496c65335d02a0c137c545a73aa2ed49e39d3d
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-04 15:35:34 -0500

    after impriment Transaction class and getter

--- index.js
+++ index.js
@@ -7,46 +7,42 @@ class Account {
   }
 }
 
-class Withdrawal {
-  // Pass in the account that the withdrawal this for
+class Transaction {
+
   constructor(amount, account) {
-    this.amount = amount;
+    this.amount  = amount;
     this.account = account;
   }
-
-  // Update the balance in the account
   commit() {
-    this.account.balance -= this.amount;
+    this.account.balance += this.value;
   }
 }
 
-class Deposit {
-   // Pass in the account that the deposit this for
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-
+class Deposit extends Transaction {
   // Update the balance in the account
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
+
+class Withdrawal extends Transaction{
+  // Update the balance in the account
+  get value() {
+    return -this.amount;
+  }
+}
+
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
